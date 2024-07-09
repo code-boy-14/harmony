a3cc993d1bc3f673fa2c0f79cb916678c93f8bab
Kawaii <xiaotskawaii@gmail.com>
2024-07-09 16:47:46 +0800

    json-server

---
+++ entry/src/main/ets/models/commodityModel.ts
@@ -0,0 +1,8 @@
+export interface commodityModel {
+  name: string
+  id: string
+  img: string
+  price: string
+  description: string
+  stock: string
+}
---
+++ entry/src/main/ets/models/requestModel.ts
@@ -0,0 +1,5 @@
+import { commodityModel } from './commodityModel'
+
+export interface ReqCommodityModel {
+  data: commodityModel[]
+}
