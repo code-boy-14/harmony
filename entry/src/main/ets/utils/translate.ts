d8a3dda92f3959f3b7fc89d97553176c9f9265e3
Tang Zijie <2585854599@qq.com>
2024-07-12 11:06:31 +0800

    修改翻译功能，但并未完成

--- entry/src/main/ets/utils/translate.ts
+++ entry/src/main/ets/utils/translate.ts
@@ -1,11 +1,18 @@
 import axios from '@ohos/axios'
 import { JSON } from '@kit.ArkTS'
+import { CommonShapeMethod } from '@ohos.arkui.shape'
+
+const token="3975l6lr5pcbvidl6jl2"
 
 //创建axios的实例
 const instance = axios.create({
   //基路径，要看API帮助文档的特征来确定基路径
-  baseURL: 'https://www.fanyigou.com/TranslateApi/api/trans',
+  baseURL: 'http://api.interpreter.caiyunai.com/v1/translator',
   timeout: 5000, //请求超时的时间
+  headers:{"content-type": "application/json",
+    "x-authorization": "token " + token},
+  method:'post',
+  // data:transPara
 })
 
 //响应拦截器，通过响应拦截器进一步对返回的数据做处理
